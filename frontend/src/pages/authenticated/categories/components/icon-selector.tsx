17eade9dda51023a648da383eb6993ca58c23f01
Miguel Rios <miguelriosoliveira@gmail.com>
2026-06-22 09:17:31 +0200

    fix: Lint changes

--- frontend/src/pages/authenticated/categories/components/icon-selector.tsx
+++ frontend/src/pages/authenticated/categories/components/icon-selector.tsx
@@ -1,9 +1,5 @@
 import { OptionSelector } from '@/components/option-selector';
-import {
-	CATEGORY_ICONS,
-	CATEGORY_LABELS,
-	type CategoryType,
-} from './category-icon';
+import { CATEGORY_ICONS, CATEGORY_LABELS, type CategoryType } from './category-icon';
 
 type Props = {
 	value: CategoryType;
@@ -21,12 +17,5 @@ const ICON_OPTIONS = (Object.keys(CATEGORY_ICONS) as CategoryType[]).map(categor
 });
 
 export function IconSelector({ value, onChange }: Props) {
-	return (
-		<OptionSelector
-			label="Ícone"
-			value={value}
-			onChange={onChange}
-			options={ICON_OPTIONS}
-		/>
-	);
+	return <OptionSelector label="Ícone" value={value} onChange={onChange} options={ICON_OPTIONS} />;
 }
